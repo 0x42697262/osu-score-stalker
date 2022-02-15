1a392139ca82e02561718df91642389bc84f741b
Birb <birb@chicken.localdomain>
2022-02-15 15:16:00 +0900

    test

---
+++ app.py
@@ -0,0 +1,15 @@
+from email.policy import default
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS
+from api.testapi import HelloApiHandler
+
+app = Flask(__name__, static_url_path="", status_folder="frontend/build")
+CORS(app)
+api = Api(app)
+
+@app.route("/", defaults={"path":""})
+def serve(path):
+    return send_from_directory(app.static_folder, "index.html")
+
+api.add_resource(HelloApiHandler, "/flask/hello")
