f7c486f1d3400af1bde616964858b1054032725f
KrulYuno <giogulane.wineffgulane@gmail.com>
2022-02-15 16:37:33 +0900

    uncommented cors

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #from email.policy import default
 from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS
+#from flask_cors import CORS
 from api.testapi import HelloApiHandler
 
 app = Flask(__name__, static_url_path="", static_folder="frontend/build")
-CORS(app)
+#CORS(app)
 api = Api(app)
 
 @app.route("/", defaults={"path":""})
